c2eaf4e2d0ec6e35a0fbcea5281353bb50a696e9
huujinn <167833461+huujinn@users.noreply.github.com>
2024-10-22 10:22:03 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -14,10 +14,10 @@ function sayhello66(){
  alert('Hello,'+who66+'!');
 }
 function taro66(){
- alert(who66+'…が来た')
+ alert(taro66+'が来た')
  who66='太郎';
 }
 function hanako66(){
- alert(who66+'…が来た')
+ alert(hanako66+'が来た')
  who66='花子';
 }
