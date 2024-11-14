1160df0180d9035c2fd39b04113a723b28b28527
huujinn <167833461+huujinn@users.noreply.github.com>
2024-11-14 20:22:46 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -2,3 +2,8 @@ function imgchange1(){
   document.getElementById('logo').src=
   './image/500.jpg';
 }
+
+function imgchange2(){
+  document.getElementById('logo').src=
+  './image/josai_logo.jpg';
+}
