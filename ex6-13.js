0a61f30f9124cc5bdb0cb6bd464e25c54046bb93
huujinn <167833461+huujinn@users.noreply.github.com>
2024-11-26 10:03:52 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -8,7 +8,7 @@ var str13="一文字ずつ表示します。";
 //テキストボックスの文字数
 var cnt13=document.timer13.moji13.value.length;
 //文字が全部表示されているか確認
-if(cnt < 11){
+if(cnt13 < 11){
 //現在より1文字多く切り出して表示
 document.timer13.moji13.value=str13.substr(0,cnt13+1);}
 else{
