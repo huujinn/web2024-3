f521e4ab1a3f933268eb71044f400c00e71c97e3
huujinn <167833461+huujinn@users.noreply.github.com>
2024-12-11 12:36:14 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -2,5 +2,6 @@ function change1(){
 document.getElementById('bulb').src=
 './image/OIP.jpg';
 }
-var chan=function()
+var chan=function(){document.getElementById('bulb').src=
+'./image/bulb-off.png'}
 setTimeout(chan,3000)
