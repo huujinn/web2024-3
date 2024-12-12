96e93892e53d46d9d188a005e476b785bdad01ae
huujinn <167833461+huujinn@users.noreply.github.com>
2024-12-12 20:30:27 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,6 +1,6 @@
 function add1(){
 var x= Number(document.getElementById('input11').value);
-var y= Number(document.getelementById('input12').value);
+var y= Number(document.getElementById('input12').value);
 //身長を変換
 x= x/100;
 //BMIの計算
