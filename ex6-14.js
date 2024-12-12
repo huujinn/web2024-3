02bf583d71fdc9ea6cdc7792058bfc32a692d22f
huujinn <167833461+huujinn@users.noreply.github.com>
2024-12-12 17:39:26 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,9 +1,7 @@
 function change1(){
-var imgElement=document.getElementById('bulb');
+var imgElement= document.getElementById('bulb').src=
+'./image/OIP.jpg';
 var bulboffSrc='./image/bulb-off.png';
-var bulbonSrc='./image/OIP.jpg';
-//新しい画像に変更
-imgElement.src=bulbonSrc; 
 //3秒後に元に戻す
 setTimeout(function(){
 imgElement.src=bulboffSrc;
