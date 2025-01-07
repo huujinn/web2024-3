7a492a72dddc65859237b13bef8993ca8b828e2c
huujinn <167833461+huujinn@users.noreply.github.com>
2025-01-07 09:45:36 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,6 @@
-
+function check(){
+var year=documment.getElementById('year').value;
+year=parseInt(year);
+if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0))
+{alert("閏年");} else {alert("平年");}
+}
